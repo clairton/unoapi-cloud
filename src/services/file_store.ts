c12a9470fc701b55e05b31b844ba55d4277a7af0
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2023-03-08 09:03:53 -0300

    remove file store when is empty

--- src/services/file_store.ts
+++ src/services/file_store.ts
@@ -3,22 +3,30 @@ import { store } from './store'
 import { DataStore } from './data_store'
 import { getFileDataStore } from './get_file_data_store'
 import { MEDIA_DIR } from './file_data_store'
+import { existsSync, readFileSync, rmSync } from 'fs'
 
 export const SESSION_DIR = './data/sessions'
 
 export const fileStore: store = async (phone) => {
   const sessionDir = `${SESSION_DIR}/${phone}`
   const mediaDir = `${MEDIA_DIR}/${phone}`
-  console.debug(`Store session in directory: ${sessionDir}`)
-  console.debug(`Store medias in directory: ${mediaDir}`)
+  console.info(`Store session in directory: ${sessionDir}`)
+  console.info(`Store medias in directory: ${mediaDir}`)
   const { state, saveCreds } = await useMultiFileAuthState(sessionDir)
-  const data: DataStore = getFileDataStore(phone, {})
+  const dataStore: DataStore = getFileDataStore(phone, {})
   const dataFile = `./data/stores/${phone}.json`
-  console.debug(`Store data in file: ${dataFile}`)
-  data.readFromFile(dataFile)
+  console.info(`Store data in file: ${dataFile}`)
+  if (existsSync(dataFile)) {
+    console.debug(`Store data in file already exist: ${dataFile}`)
+    const content = readFileSync(dataFile)
+    if (!content.toString()) {
+      console.debug(`Store data in file content is empty and was removed: ${dataFile}`)
+      rmSync(dataFile)
+    }
+  }
+  dataStore.readFromFile(dataFile)
   setInterval(() => {
-    data.writeToFile(dataFile), 10_0000
+    dataStore.writeToFile(dataFile), 10_0000
   })
-
-  return { state, saveCreds, dataStore: data }
+  return { state, saveCreds, dataStore }
 }
