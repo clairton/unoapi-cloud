f55925bc00a6173e6956258310ccee53aa2f7bc2
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2023-03-15 08:32:56 -0300

    logs in ping

--- src/controllers/index_controller.ts
+++ src/controllers/index_controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express'
 
 class IndexController {
-  public ping(_req: Request, res: Response) {
+  public ping(req: Request, res: Response) {
+    console.debug('ping headers', req.headers)
+    console.debug('ping params', req.params)
+    console.debug('ping body', JSON.stringify(req.body, null, ' '))
     res.set('Content-Type', 'text/plain')
     return res.status(200).send('pong!')
   }
