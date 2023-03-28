7984d984420f2412495fab369007648767b28579
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2023-03-27 21:00:09 -0300

    getMedia with return void

--- src/services/media_store.ts
+++ src/services/media_store.ts
@@ -9,7 +9,7 @@ export interface getMediaStore {
 }
 
 export type MediaStore = {
-  getMedia: (baseUrl: string, mediaId: string) => Promise<object>
+  getMedia: (baseUrl: string, mediaId: string) => Promise<object | void>
   saveMedia: (waMessage: WAMessage) => Promise<boolean>
   removeMedia: (fileName: string) => Promise<void>
   downloadMedia: (resp: Response, fileName: string) => Promise<void>
