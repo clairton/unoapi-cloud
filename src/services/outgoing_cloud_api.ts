3ec0525d798d68e88d5778749e95aeced3c28d9a
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2023-04-18 16:08:05 -0300

    get message type in shoulIgnoreKey

--- src/services/outgoing_cloud_api.ts
+++ src/services/outgoing_cloud_api.ts
@@ -16,8 +16,7 @@ export class OutgoingCloudApi implements Outgoing {
     const config = await this.getConfig(phone)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const filteredMessages = messages.filter((m: any) => {
-      const messageType = getMessageType(m)
-      return m.key && !config.shouldIgnoreJid(m.key.remoteJid) && !config.shouldIgnoreKey(m.key, messageType)
+      return m.key && m.key.remoteJid && !config.shouldIgnoreJid(m.key.remoteJid) && !config.shouldIgnoreKey(m.key, getMessageType(m))
     })
     console.debug('%s filtereds messages/updates of %s', messages.length - filteredMessages.length, messages.length)
     await Promise.all(filteredMessages.map(async (m: object) => this.sendOne(phone, m)))
