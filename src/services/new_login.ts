ccb88247f34dac4c18be484517b95d8f8235c624
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2023-10-13 07:14:20 -0300

    format

--- src/services/new_login.ts
+++ src/services/new_login.ts
@@ -4,7 +4,8 @@ import { OnNewLogin } from './socket'
 import { phoneNumberToJid } from './transformer'
 
 // eslint-disable-next-line
-export const onNewLogin = (outgoing: Outgoing): OnNewLogin =>
+export const onNewLogin =
+  (outgoing: Outgoing): OnNewLogin =>
   (phone: string): Promise<void> => {
     const message = `Please be careful, the http endpoint is unprotected and if it is exposed in the network, someone else can send message as you!`
     const payload = {
