90bc365c1db268f8da3261462732b4ff4a460fb8
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-01-09 06:42:57 -0300

    json logs on /ping

--- src/controllers/index_controller.ts
+++ src/controllers/index_controller.ts
@@ -3,9 +3,9 @@ import logger from '../services/logger'
 
 class IndexController {
   public ping(req: Request, res: Response) {
-    logger.debug('ping method %s', req.method)
-    logger.debug('ping headers %s', req.headers)
-    logger.debug('ping params %s', req.params)
+    logger.debug('ping method %s', JSON.stringify(req.method))
+    logger.debug('ping headers %s', JSON.stringify(req.headers))
+    logger.debug('ping params %s', JSON.stringify(req.params))
     logger.debug('ping body %s', JSON.stringify(req.body))
     res.set('Content-Type', 'text/plain')
     return res.status(200).send('pong!')
