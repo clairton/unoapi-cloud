13e8d04882fc03f754767056a52cf2f548227ecf
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-01-24 21:47:58 -0300

    add routes to compability with typebot requests

--- src/controllers/index_controller.ts
+++ src/controllers/index_controller.ts
@@ -10,6 +10,37 @@ class IndexController {
     res.set('Content-Type', 'text/plain')
     return res.status(200).send('pong!')
   }
+
+  public debugToken(req: Request, res: Response) {
+    logger.debug('debug token method %s', JSON.stringify(req.method))
+    logger.debug('debug token headers %s', JSON.stringify(req.headers))
+    logger.debug('debug token params %s', JSON.stringify(req.params))
+    logger.debug('debug token query %s', JSON.stringify(req.query))
+    logger.debug('debug token body %s', JSON.stringify(req.body))
+    res.set('Content-Type', 'application/json')
+    return res.status(200).send({
+      data: {
+        is_valid: true,
+        app_id: 'unoapi',
+        application: 'unoapi',
+        expires_at: 0,
+        scopes: ['whatsapp_business_management', 'whatsapp_business_messaging'],
+      },
+    })
+  }
+
+  public getPhoneNumber(req: Request, res: Response) {
+    logger.debug('get phone number method %s', JSON.stringify(req.method))
+    logger.debug('get phone number headers %s', JSON.stringify(req.headers))
+    logger.debug('get phone number params %s', JSON.stringify(req.params))
+    logger.debug('get phone number query %s', JSON.stringify(req.query))
+    logger.debug('get phone number body %s', JSON.stringify(req.body))
+    res.set('Content-Type', 'application/json')
+    const { phone } = req.params
+    return res.status(200).send({
+      display_phone_number: phone,
+    })
+  }
 }
 
 export const indexController = new IndexController()
