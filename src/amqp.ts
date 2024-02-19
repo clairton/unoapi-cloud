669bbeaa8c55f16d375d4b08af13d73ec3613cd6
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-02-19 11:31:44 -0300

    log ignore message

--- src/amqp.ts
+++ src/amqp.ts
@@ -178,18 +178,17 @@ export const amqpConsume = async (
     }
     const content: string = payload.content.toString()
     const phone = payload.fields.routingKey
-    if (IGNORED_CONNECTIONS_NUMBERS.includes(phone)) {
-      logger.info(`Ignore messages from ${phone}`)
-      await channel.ack(payload)
-      return
-    }
     const data = JSON.parse(content)
     const headers = payload.properties.headers || {}
     const maxRetries = parseInt(headers[UNOAPI_X_MAX_RETRIES] || UNOAPI_MESSAGE_RETRY_LIMIT)
     const countRetries = parseInt(headers[UNOAPI_X_COUNT_RETRIES] || '0') + 1
     try {
       logger.debug('Received in %s with phone: %s, message: %s with headers: %s', queue, phone, content, JSON.stringify(payload.properties.headers))
-      await callback(phone, data, { countRetries, maxRetries })
+      if (IGNORED_CONNECTIONS_NUMBERS.includes(phone)) {
+        logger.info(`Ignore messages from ${phone}`)
+      } else {
+        await callback(phone, data, { countRetries, maxRetries })
+      }
       logger.debug('Ack message!')
       await channel.ack(payload)
     } catch (error) {
