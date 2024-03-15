dbe35214b350cde2a15ebeceda838f67b44ca784
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-03-14 23:23:54 -0300

    timeout to session state connecting

--- src/services/session_store.ts
+++ src/services/session_store.ts
@@ -1,10 +1,19 @@
+import { CONNECTING_TIMEOUT_MS } from '../defaults'
 import logger from './logger'
+
 const statuses: Map<string, string> = new Map<string, string>()
 
 export const getSessionStatus = async (phone: string) => statuses.get(phone) || 'disconnected'
 
 export const setSessionStatus = async (phone: string, status: 'offline' | 'online' | 'disconnected' | 'connecting') => {
   logger.info(`Session status ${phone} change from ${await getSessionStatus(phone)} to ${status}`)
+  if (status == 'connecting') {
+    setTimeout(() => {
+      if (statuses.get(phone) == 'connecting') {
+        statuses.set(phone, 'disconnected')
+      }
+    }, CONNECTING_TIMEOUT_MS)
+  }
   statuses.set(phone, status)
 }
 
