f8289397283359a911d1af45ddcb766c89fea1f9
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-03-15 17:47:04 -0300

    Status attemp set 1 on close

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -239,6 +239,7 @@ export const connect = async ({
 
     await setSessionStatus(phone, 'offline')
     logger.info(`${phone} close`)
+    status.attempt = 1
     try {
       await sock?.ws?.close()
     } catch (error) {}
