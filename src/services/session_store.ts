ad01ba8e4b79c6ad699e98b06e2ce076ede5d80a
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-03-18 20:00:56 -0300

    timeout for qrcode

--- src/services/session_store.ts
+++ src/services/session_store.ts
@@ -1,15 +1,18 @@
 import { CONNECTING_TIMEOUT_MS } from '../defaults'
 import logger from './logger'
 
+const TIMEOUT_STATUSES = ['qrcode', 'connecting']
+
 const statuses: Map<string, string> = new Map<string, string>()
 
 export const getSessionStatus = async (phone: string) => statuses.get(phone) || 'disconnected'
 
 export const setSessionStatus = async (phone: string, status: 'offline' | 'online' | 'disconnected' | 'connecting' | 'qrcode') => {
   logger.info(`Session status ${phone} change from ${await getSessionStatus(phone)} to ${status}`)
-  if (status == 'connecting') {
+  if (TIMEOUT_STATUSES.includes(status)) {
     setTimeout(() => {
-      if (statuses.get(phone) == 'connecting') {
+      const s = statuses.get(phone)
+      if (s && TIMEOUT_STATUSES.includes(s)) {
         logger.info('Session %s status connecting timeout config to %s', phone, CONNECTING_TIMEOUT_MS)
         statuses.set(phone, 'disconnected')
       }
@@ -20,7 +23,7 @@ export const setSessionStatus = async (phone: string, status: 'offline' | 'onlin
 }
 
 export const isSessionStatusOnline = async (phone: string) => (await getSessionStatus(phone)) == 'online'
-export const isSessionStatusConnecting = async (phone: string) => (await getSessionStatus(phone)) == 'connecting'
+export const isSessionStatusConnecting = async (phone: string) => TIMEOUT_STATUSES.includes(await getSessionStatus(phone))
 export const isSessionStatusOffline = async (phone: string) => (await getSessionStatus(phone)) == 'offline'
 export const isSessionStatusIsDisconnect = async (phone: string) => (await getSessionStatus(phone)) == 'disconnected'
 
