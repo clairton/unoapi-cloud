e1c2f0fbfbb72a2f14449ad83b0bef0566191074
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-06-15 15:54:57 -0300

    blacklist param to is required

--- src/controllers/blacklist_controller.ts
+++ src/controllers/blacklist_controller.ts
@@ -15,6 +15,9 @@ export class BlacklistController {
     logger.debug('blacklist params %s', JSON.stringify(req.params))
     logger.debug('blacklist body %s', JSON.stringify(req.body))
     const { to, ttl } = req.body
+    if (!to) {
+      return res.status(400).send(`{"error": "to param is required"}`)
+    }
     const { webhook_id, phone } = req.params
     await this.addToBlacklist(phone, webhook_id, to, ttl)
     res.status(200).send(`{"success": true}`)
