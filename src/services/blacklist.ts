ff4e44a6da8ed6f8434f727ce680bf2be798e3a0
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-06-19 09:05:54 -0300

    fix get phone in payload

--- src/services/blacklist.ts
+++ src/services/blacklist.ts
@@ -18,10 +18,14 @@ export interface isInBlacklist {
 export const extractDestinyPhone = (payload: object) => {
   const data = payload as any
   const number = (data?.to || (
-    data?.entry
-    && data?.entry[0]?.changes
-    && data?.entry[0]?.changes[0]?.value?.contacts[0]
-    && data?.entry[0]?.changes[0]?.value?.contacts[0]?.wa_id)?.replace('+', '')
+    data.entry
+    && data.entry[0]
+    && data.entry[0].changes
+    && data.entry[0].changes[0]
+    && data.entry[0].changes[0].value
+    && data.entry[0].changes[0].value.contacts
+    && data.entry[0].changes[0].value.contacts[0]
+    && data.entry[0].changes[0].value.contacts[0].wa_id)?.replace('+', '')
   )
   if (!number) {
     throw Error(`error on get phone number from ${JSON.stringify(payload)}`)
