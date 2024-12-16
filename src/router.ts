326ee55062633b95c620537d821505f88279365d
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-12-16 07:11:54 -0300

    add /phone_numbers again

--- src/router.ts
+++ src/router.ts
@@ -53,6 +53,8 @@ export const router = (
   router.post('/:version/:phone/register', middleware, registrationController.register.bind(registrationController))
   router.post('/:version/:phone/deregister', middleware, registrationController.deregister.bind(registrationController))
   router.get('/:version/:phone', middleware, phoneNumberController.get.bind(phoneNumberController))
+  // https://developers.facebook.com/docs/whatsapp/business-management-api/manage-phone-numbers/
+  router.get('/:version/:phone/phone_numbers', middleware, phoneNumberController.list.bind(phoneNumberController))
   router.get('/:version/:phone/message_templates', middleware, templatesController.index.bind(templatesController))
   router.post('/:version/:phone/templates', middleware, templatesController.templates.bind(templatesController))
   router.post('/:version/:phone/messages', middleware, messagesController.index.bind(messagesController))
