1dfeb27ac655e88a5615df9eceaeba1e8a3436b4
rodrigo-gmengue <rogm778@gmail.com>
2025-02-11 20:11:35 -0300

    ajustes

--- examples/scripts/envSetup.sh
+++ examples/scripts/envSetup.sh
@@ -15,172 +15,6 @@ validate_domain() {
     fi
 }
 
-minio_setup() {
-    echo "Iniciando a configuração do MinIO..."
-    echo "Digite a região configurada no MinIO:"
-    read MINIO_REGION
-    echo "Digite o nome do bucket para o UnoAPI:"
-    read UNOAPI_BUCKET
-    echo "Digite o Access Key do MinIO:"
-    read MINIO_ACCESS_KEY
-    echo "Digite o Secret Key do MinIO:"
-    read MINIO_SECRET_KEY
-    echo "Digite o nome do bucket para o Chatwoot:"
-    read CW_BUCKET
-
-    # Adiciona as variáveis ao .env
-    cat >> .env <<EOL
-MINIO_REGION=$MINIO_REGION
-UNOAPI_BUCKET=$UNOAPI_BUCKET
-MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY
-MINIO_SECRET_KEY=$MINIO_SECRET_KEY
-CW_BUCKET=$CW_BUCKET
-EOL
-
-    echo "Variáveis adicionadas ao .env com sucesso."
-    genUnoapiStack
-
-    setup
-    exit 0
-    
-}
-
-genUnoapiStack() {
-
-    # Dados do repositório alvo
-    REPO_OWNER="clairton"
-    REPO_NAME="unoapi-cloud"
-
-    # Obtém a última tag usando a API do GitHub
-    LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/tags" | jq -r '.[0].name' | cut -c2-)
-
-    # Verifica se encontrou uma tag
-    if [[ -z "$LATEST_TAG" || "$LATEST_TAG" == "null" ]]; then
-        echo "Nenhuma tag encontrada no repositório remoto."
-        exit 1
-    fi
-
-    echo "Você quer Gerar uma stack para a versão $LATEST_TAG da unoapi? 
-    Se sim: Aperte Enter, 
-    caso deseje: Digite a versão que quer utilizar"
-    read unoapi_version
-    unoapi_version=${unoapi_version:-$LATEST_TAG}
-
-    echo "Criando Stack para a Versão $unoapi_image"
-
-    # Recarregar variáveis
-    export $(grep -v '^#' .env | xargs)
-    export UNOAPI_IMAGE="clairton/unoapi-cloud:$unoapi_image"
-
-    # Baixar o arquivo docker-compose-model.yaml do GitHub
-    curl -fsSL https://raw.githubusercontent.com/rodrigo-gmengue/unoapi-cloud/refs/heads/tutorials/examples/scripts/uno-model.yaml -o uno-model.yaml
-
-    # Substituir as variáveis no arquivo docker-compose-model.yaml
-    envsubst < uno-model.yaml > docker-unoapi.yaml
-
-    echo "Arquivo docker-unoapi.yaml gerado com sucesso."
-    
-    setup
-    exit 0
-    
-}
-
-genCwStack() {
-    # Dados do repositório alvo
-    REPO_OWNER="clairton"
-    REPO_NAME="chatwoot"
-
-    # Obtém a última tag usando a API do GitHub
-    LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/tags" | jq -r '.[0].name')
-
-    # Verifica se encontrou uma tag
-    if [[ -z "$LATEST_TAG" || "$LATEST_TAG" == "null" ]]; then
-        echo "Nenhuma tag encontrada no repositório remoto."
-        exit 1
-    fi
-
-    echo "Você quer Gerar uma stack para a versão $LATEST_TAG do chatwoot uno? 
-    Se sim: Aperte Enter, 
-    caso deseje: Digite a versão que quer utilizar"
-    read cw_version
-    cw_version=${cw_version:-$LATEST_TAG}
-
-    echo "Criando Stack para a Versão $cw_version do chatwoot uno"
-
-    # Recarregar variáveis
-    export $(grep -v '^#' .env | xargs)
-    export CHATWOOT_IMAGE="clairton/chatwoot:$unoapi_image"
-
-    # Baixar o arquivo docker-compose-model.yaml do GitHub
-    curl -fsSL https://raw.githubusercontent.com/rodrigo-gmengue/unoapi-cloud/refs/heads/tutorials/examples/scripts/chatwoot-model.yaml -o chatwoot-model.yaml
-
-    # Substituir as variáveis no arquivo docker-compose-model.yaml
-    envsubst < chatwoot-model.yaml > docker-chatwoot.yaml
-
-    echo "Arquivo docker-chatwoot.yaml gerado com sucesso."
-    
-    setup
-    exit 0
-}
-
-setup(){
-    # SETUP PRINCIPAL
-    if [ -f ".env" ]; then
-        echo "Arquivo .env já existe. O que você gostaria de fazer?
-        1 - Configurar as Opções para o Minio
-        2 - Gerar Stack Atualizada da Unoapi
-        3 - Gerar Stack Atualizada do Chatwoot Uno
-        0 - Gerar novo .env"
-        read resposta
-
-        if [ "$resposta" = "1" ]; then
-            echo "Configurar Minio..."
-            minio_setup
-        fi
-        if [ "$resposta" = "2" ]; then
-            echo "Configurando Unoapi..."
-            genUnoapiStack
-        fi
-        if [ "$resposta" = "3" ]; then
-            echo "Configurado Chatwoot UNO..."
-            genCwStack
-        fi
-        if [ "$resposta" = "0" ]; then
-            echo "Setup Inicial..."
-            initialSetup
-        else
-            echo "Não compreendi sua resposta, Caso queira sair aperte CTRL + C"
-            echo ""
-            setup
-        fi
-        
-    else
-        echo "Bem vindo ao AutoConfigurar Stack para a Unoapi e Chatwoot UNO
-        Vamos começar configurando algumas variáveis?"
-        initialSetup
-        exit 0
-    fi
-}
-
-#Valida para opções passada por parâmetro
-if [ "$1" = "minio" ]; then
-    minio_setup
-    exit 0
-fi
-if [ "$1" = "genUnoStack" ]; then
-    genUnoapiStack
-    exit 0
-fi
-
-if [ "$1" = "genCwStack" ]; then
-    genCwStack
-    exit 0
-fi
-if [ "$1" = "" ]; then
-    setup
-    exit 0
-fi
-
 
 initialSetup(){
     echo "Qual o nome da rede Docker padrão?"
@@ -262,6 +96,10 @@ initialSetup(){
     read unoapi_auth_token
     unoapi_auth_token=${unoapi_auth_token:-$(generate_random_string)}
 
+    echo "Nome do Banco de Dados para o Chatwoot (padrão: chatwoot_db):"
+    read POSTGRES_DB
+    POSTGRES_DB=${POSTGRES_DB:-"chatwoot_db"}
+
     # Confirmar se as variáveis estão corretas
     echo ""
     echo "Por favor, revise as configurações:"
@@ -286,8 +124,9 @@ initialSetup(){
 
     read confirm
     if [ "$confirm" != "y" ]; then
-        echo "Você pode editar o arquivo .env diretamente ou reiniciar o script."
-        exit 1
+        echo ""
+        initialSetup
+        exit 0
     fi
 
     # Criar o arquivo de envs
@@ -311,6 +150,7 @@ UNOAPI_AUTH_TOKEN=$unoapi_auth_token
 MINIOWEB_SUBDOMAIN=$minioweb_subdomain
 MINIOAPI_SUBDOMAIN=$minioapi_subdomain
 DOCKERNETWORK=$dockernetwork
+POSTGRES_DB=$POSTGRES_DB
 EOL
 
     echo "Arquivo .env gerado com sucesso."
@@ -328,7 +168,174 @@ EOL
 
     echo ""
     echo "Execute o comando docker compose up -d para iniciar os serviços!"
+    rm docker-model.yaml
+    setup
+    exit 0
+}
+
+minio_setup() {
+    echo "Iniciando a configuração do MinIO..."
+    echo "Digite a região configurada no MinIO:"
+    read MINIO_REGION
+    echo "Digite o nome do bucket para o UnoAPI:"
+    read UNOAPI_BUCKET
+    echo "Digite o Access Key do MinIO:"
+    read MINIO_ACCESS_KEY
+    echo "Digite o Secret Key do MinIO:"
+    read MINIO_SECRET_KEY
+    echo "Digite o nome do bucket para o Chatwoot:"
+    read CW_BUCKET
+
+    # Adiciona as variáveis ao .env
+    cat >> .env <<EOL
+MINIO_REGION=$MINIO_REGION
+UNOAPI_BUCKET=$UNOAPI_BUCKET
+MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY
+MINIO_SECRET_KEY=$MINIO_SECRET_KEY
+CW_BUCKET=$CW_BUCKET
+EOL
+
+    echo "Variáveis adicionadas ao .env com sucesso."
+    genUnoapiStack
+
+    setup
+    exit 0
+    
+}
+
+genUnoapiStack() {
+
+    # Dados do repositório alvo
+    REPO_OWNER="clairton"
+    REPO_NAME="unoapi-cloud"
+
+    # Obtém a última tag usando a API do GitHub
+    LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/tags" | jq -r '.[0].name' | cut -c2-)
+
+    # Verifica se encontrou uma tag
+    if [ -z "$LATEST_TAG" ] || [ "$LATEST_TAG" = "null" ]; then
+        echo "Nenhuma tag encontrada no repositório remoto."
+        exit 1
+    fi
+
+    echo "Você quer Gerar uma stack para a versão $LATEST_TAG da unoapi? 
+    Se sim: Aperte Enter, 
+    caso deseje: Digite a versão que quer utilizar"
+    read unoapi_version
+    unoapi_version=${unoapi_version:-$LATEST_TAG}
+
+    echo "Criando Stack para a Versão $unoapi_version da unoapi"
+
+    # Recarregar variáveis
+    export $(grep -v '^#' .env | xargs)
+    export UNOAPI_IMAGE="clairton/unoapi-cloud:$unoapi_version"
+
+    # Baixar o arquivo docker-compose-model.yaml do GitHub
+    curl -fsSL https://raw.githubusercontent.com/rodrigo-gmengue/unoapi-cloud/refs/heads/tutorials/examples/scripts/uno-model.yaml -o uno-model.yaml
+
+    # Substituir as variáveis no arquivo docker-compose-model.yaml
+    envsubst < uno-model.yaml > docker-unoapi.yaml
+
+    echo "Arquivo docker-unoapi.yaml gerado com sucesso."
+    rm uno-model.yaml
+    setup
+    exit 0
+    
+}
+
+genCwStack() {
+    # Dados do repositório alvo
+    REPO_OWNER="clairton"
+    REPO_NAME="chatwoot"
+
+    # Obtém a última tag usando a API do GitHub
+    LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/tags" | jq -r '.[0].name')
+
+    # Verifica se encontrou uma tag
+    if [ -z "$LATEST_TAG" ] || [ "$LATEST_TAG" = "null" ]; then
+        echo "Nenhuma tag encontrada no repositório remoto."
+        exit 1
+    fi
+
+    echo "Você quer Gerar uma stack para a versão $LATEST_TAG do chatwoot uno? 
+    Se sim: Aperte Enter, 
+    caso deseje: Digite a versão que quer utilizar"
+    read cw_version
+    cw_version=${cw_version:-$LATEST_TAG}
+
+    echo "Criando Stack para a Versão $cw_version do chatwoot uno"
+
+    # Recarregar variáveis
+    export $(grep -v '^#' .env | xargs)
+    export CHATWOOT_IMAGE="clairton/chatwoot:$cw_version"
+
+    # Baixar o arquivo docker-compose-model.yaml do GitHub
+    curl -fsSL https://raw.githubusercontent.com/rodrigo-gmengue/unoapi-cloud/refs/heads/tutorials/examples/scripts/chatwoot-model.yaml -o chatwoot-model.yaml
+
+    # Substituir as variáveis no arquivo docker-compose-model.yaml
+    envsubst < chatwoot-model.yaml > docker-chatwoot.yaml
+
+    echo "Arquivo docker-chatwoot.yaml gerado com sucesso."
+    rm chatwoot-model.yaml
     
     setup
     exit 0
 }
+
+setup(){
+    # SETUP PRINCIPAL
+    if [ -f ".env" ]; then
+        echo "Arquivo .env já existe. O que você gostaria de fazer?
+        1 - Configurar as Opções para o Minio
+        2 - Gerar Stack Atualizada da Unoapi
+        3 - Gerar Stack Atualizada do Chatwoot Uno
+        0 - Gerar novo .env"
+        read resposta
+
+        if [ "$resposta" = "1" ]; then
+            echo "Configurar Minio..."
+            minio_setup
+        fi
+        if [ "$resposta" = "2" ]; then
+            echo "Configurando Unoapi..."
+            genUnoapiStack
+        fi
+        if [ "$resposta" = "3" ]; then
+            echo "Configurado Chatwoot UNO..."
+            genCwStack
+        fi
+        if [ "$resposta" = "0" ]; then
+            echo "Setup Inicial..."
+            initialSetup
+        else
+            echo "Não compreendi sua resposta, Caso queira sair aperte CTRL + C"
+            echo ""
+            setup
+        fi
+        
+    else
+        echo "Bem vindo ao AutoConfigurar Stack para a Unoapi e Chatwoot UNO
+        Vamos começar configurando algumas variáveis?"
+        initialSetup
+        exit 0
+    fi
+}
+
+#Valida para opções passada por parâmetro
+if [ "$1" = "minio" ]; then
+    minio_setup
+    exit 0
+fi
+if [ "$1" = "genUnoStack" ]; then
+    genUnoapiStack
+    exit 0
+fi
+
+if [ "$1" = "genCwStack" ]; then
+    genCwStack
+    exit 0
+fi
+if [ "$1" = "" ]; then
+    setup
+    exit 0
+fi
