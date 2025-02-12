f0a20bf89399eb3737b75de01e7931f59241f7a3
rodrigo-gmengue <rogm778@gmail.com>
2025-02-12 09:49:11 -0300

    ajustes para configurar docker

--- examples/scripts/serverSetup.sh
+++ examples/scripts/serverSetup.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-# Atualizar os pacotes do sistema
-apt-get update && apt-get upgrade -y
-apt-get install ca-certificates curl gnupg nmon wget jp htop -y 
+apt update && apt upgrade -y
+apt install -y ca-certificates curl gnupg wget jp lsb-release
 
-# Instalar a chave GPG do Docker
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Adicionar o repositório Docker para Debian ou Ubuntu
 if [ -f /etc/os-release ]; then
     . /etc/os-release
-    if [[ "$ID" == "debian" ]]; then
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-        $VERSION_CODENAME stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    elif [[ "$ID" == "ubuntu" ]]; then
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    if [ "$ID" = "debian" ]; then
+
+        echo "Instalação em ambiente Debian"
+        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+        chmod a+r /etc/apt/trusted.gpg.d/docker.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    elif [ "$ID" = "ubuntu" ]; then
+        echo "Instalação em ambiente UBUNTU"
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --yes /etc/apt/trusted.gpg.d/docker.gpg
+        chmod a+r /etc/apt/trusted.gpg.d/docker.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     fi
 fi
 
-# Atualizar o repositório e instalar o Docker
-apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt update && apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 echo "Digite o nome para a rede da sua instalação docker:"
 read dockerNetwork
 dockerNetwork=${dockerNetwork:-"network"}
 
 docker network create $dockerNetwork
+
+echo "Ambiente configurado com sucesso!"
