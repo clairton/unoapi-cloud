0bca9232e83fe6c1c6a7f48b9f86a3d43b5725e8
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2025-04-10 18:08:16 -0300

    send broadcast amqp with phone number

--- src/services/broadcast_amqp.ts
+++ src/services/broadcast_amqp.ts
@@ -5,6 +5,6 @@ import { Broadcast } from './broadcast'
 export class BroadcastAmqp extends Broadcast {
   public async send(phone: string, type: string, content: string) {
     const payload = { phone, type, content }
-    await amqpPublish(UNOAPI_EXCHANGE_BROKER_NAME, UNOAPI_QUEUE_BROADCAST, '', payload)
+    await amqpPublish(UNOAPI_EXCHANGE_BROKER_NAME, UNOAPI_QUEUE_BROADCAST, phone, payload)
   }
 }
