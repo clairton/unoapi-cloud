f6ceb94152d35c5f2d7cba9129160bf8e15d8a3c
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2025-09-03 20:25:30 -0300

    jest and ts config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      useESM: true,
+    }]
+  },
 };
