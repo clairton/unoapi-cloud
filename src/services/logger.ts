211f508aecf1a5da344a56ac49e0fe0ab23adf72
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2025-10-06 09:05:45 -0300

    save log in file too

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -2,7 +2,7 @@ import P, { Level } from 'pino'
 
 import { UNO_LOG_LEVEL } from '../defaults'
 
-const logger = P({ timestamp: () => `,"time":"${new Date().toJSON()}"` })
+const logger = P({ timestamp: () => `,"time":"${new Date().toJSON()}"` }, P.destination('./data/logs/unoapi.log'))
 logger.level = UNO_LOG_LEVEL as Level
 
 export default logger
