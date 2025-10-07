9572ce23b6fbf0f856f2d68b43331c25d6c76e2d
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2025-10-07 07:44:34 -0300

    multi transport pino logger

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,8 +1,20 @@
-import P, { Level } from 'pino'
-
+import P from 'pino'
 import { UNO_LOG_LEVEL } from '../defaults'
 
-const logger = P({ timestamp: () => `,"time":"${new Date().toJSON()}"` }, P.destination('./data/logs/unoapi.log'))
-logger.level = UNO_LOG_LEVEL as Level
+const multiTransport = P.transport({
+  targets: [
+    {
+      level: UNO_LOG_LEVEL,
+      target: 'pino/file',
+      options: { destination: './data/logs/unoapi.log' }
+    },
+    {
+      level: UNO_LOG_LEVEL,
+      target: 'pino-pretty',
+      options: { colorize: true }
+    }
+  ]
+})
+const logger = P({ timestamp: () => `,"time":"${new Date().toJSON()}"` }, multiTransport)
 
 export default logger
