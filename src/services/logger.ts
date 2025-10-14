6f7a109fa9b22d59007f7accffb299ff3eeedfaf
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2025-10-14 14:03:43 -0300

    add rotate logs

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -4,9 +4,14 @@ import { UNO_LOG_LEVEL } from '../defaults'
 const multiTransport = P.transport({
   targets: [
     {
+      target: 'pino-roll',
       level: UNO_LOG_LEVEL,
-      target: 'pino/file',
-      options: { destination: './data/logs/unoapi.log' },
+      options: { 
+        destination: './data/logs/unoapi.log',
+        frequency: '1d',       // Rotate daily ('1d' for one day)
+        size: '10MB',          // Optional: Rotate also at 10MB file size
+        mkdir: true            // Create the logs directory if it doesn't exist
+      }
     },
     {
       level: UNO_LOG_LEVEL,
