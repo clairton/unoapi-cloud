5d052284b0a57d67e3917a5f8a7c8ba23b7ef52b
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2026-07-03 16:40:27 -0300

    WEBHOOK_URL with UNOAPI_UR

--- src/defaults.ts
+++ src/defaults.ts
@@ -39,7 +39,7 @@ export const WEBHOOK_FORWARD_TIMEOUT_MS = parseInt(process.env.WEBHOOK_TIMEOUT_M
 // comunication
 export const UNOAPI_URL = process.env.UNOAPI_URL || 'http://localhost:9876'
 export const WEBHOOK_URL_ABSOLUTE = process.env.WEBHOOK_URL_ABSOLUTE || ''
-export const WEBHOOK_URL = process.env.WEBHOOK_URL || 'http://localhost:9876/webhooks/fake'
+export const WEBHOOK_URL = process.env.WEBHOOK_URL || `${UNOAPI_URL}/webhooks/fake`
 export const WEBHOOK_HEADER = process.env.WEBHOOK_HEADER || 'Authorization'
 export const WEBHOOK_TOKEN = process.env.WEBHOOK_TOKEN || UNOAPI_AUTH_TOKEN || '123abc'
 export const WEBHOOK_TIMEOUT_MS = parseInt(process.env.WEBHOOK_TIMEOUT_MS || '360000')
